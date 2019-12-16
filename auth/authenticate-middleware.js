a8f1062681e959364b60fb7dcf9d6b8613a3efb0
milano12345 <dmilano857@gmail.com>
2019-12-15 21:40:14 -0600

    completed auth middleware

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,16 @@
   before granting access to the next middleware/route handler
 */
 
-module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
-};
+function authMiddleWare(req, res, next) {
+  const token = req.headers.authorization;
+  jwt.verify(token, secret, (error, decodedToken) => {
+    if (error) {
+      res.status(401).json({ message: error });
+    } else {
+      res.locals.decodedToken = decodedToken;
+      next();
+    }
+  });
+}
+
+module.exports = authMiddleWare;
